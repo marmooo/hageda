cad53d0489af6485fd113414b9e61ba9580195af
marmooo <marmooo@users.noreply.github.com>
2023-06-17 01:00:19 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-05-23 09:45";
+var CACHE_NAME = "2023-06-17 01:00";
 var urlsToCache = [
   "/hageda/",
   "/hageda/index.js",
@@ -12,7 +12,6 @@ var urlsToCache = [
   "/hageda/mp3/keyboard.mp3",
   "/hageda/favicon/favicon.svg",
   "https://marmooo.github.io/fonts/textar-light.woff2",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/simple-keyboard@3.4.52/build/index.min.js",
 ];
 
