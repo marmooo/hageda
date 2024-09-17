f0848e08b9f2a63ca45feb22cc70a5fe5c55c78d
marmooo <marmooo@users.noreply.github.com>
2024-09-18 06:09:42 +0900

    use TextLineStream

--- validate.js
+++ validate.js
@@ -1,9 +1,12 @@
-import { readLines } from "https://deno.land/std/io/mod.ts";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
 
 for (let i = 1; i <= 3; i++) {
   let lineno = 1;
-  const fileReader = await Deno.open("src/" + i + ".xml");
-  for await (const line of readLines(fileReader)) {
+  const file = await Deno.open(`src/data/${i}.xml`);
+  const lineStream = file.readable
+    .pipeThrough(new TextDecoderStream())
+    .pipeThrough(new TextLineStream());
+  for await (const line of lineStream) {
     const str = line.replace(/<[a-z/]*>/g, "");
     if (str.match(/[<>]/) && lineno != 1) {
       console.log(i + ".xml: " + lineno + ": " + line);
